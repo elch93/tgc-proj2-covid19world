08f31d8b878e552f2608d3365a63159fae3f02b5
elch93 <eugeneliong@hotmail.com>
2020-03-30 15:18:22 +0800

    added search by date functionality

--- myfunctions.js
+++ myfunctions.js
@@ -3,38 +3,20 @@
 
 
 let countryname = "Singapore" //SG by default
+let todaydate = new Date()
+console.log(todaydate)
+let loaddate = moment(todaydate).subtract(1,"day")
+loaddate = moment(loaddate).format("DD/MM/YY")
+
+console.log(loaddate)
 
 //load latest info
 function loadLatest() {
-    $("#ctoday").empty(), $("#rtoday").empty(), $("#dtoday").empty(), 
-    $("#datedisplay").empty(), $("#totalconfirmed").empty(), $("#totalrecovered").empty(), $("#totaldeaths").empty();
+    $("#ctoday").empty(), $("#rtoday").empty(), $("#dtoday").empty(),
+        $("#datedisplay").empty(), $("#totalconfirmed").empty(), $("#totalrecovered").empty(), $("#totaldeaths").empty();
     axios.get("https://pomber.github.io/covid19/timeseries.json").then(function (response) {
         //get the daily data
         let countrydata = response.data[`${countryname}`].reverse()
-        let dailyIncrease = parseInt(countrydata[0].confirmed) - parseInt(countrydata[1].confirmed)
-        let dailyRecovered = parseInt(countrydata[0].recovered) - parseInt(countrydata[1].recovered)
-        let dailyDeaths = parseInt(countrydata[0].deaths) - parseInt(countrydata[1].deaths)
-        //console.log(countrydata)
-
-        $("#ctoday").append(`${dailyIncrease}`)
-        if (dailyIncrease > 0) {
-            $("#ctoday").append(`<i class="fas fa-angle-double-up red"></i>`)
-        }
-
-        $("#rtoday").append(`${dailyRecovered}`)
-        if (dailyRecovered > 0) {
-            $("#rtoday").append(`<i class="fas fa-angle-double-up green"></i>`)
-        }
-
-        $("#dtoday").append(`${dailyDeaths}`)
-        if (dailyDeaths > 0) {
-            $("#dtoday").append(`<i class="fas fa-angle-double-up red"></i>`)
-        }
-
-        $("#totalconfirmed").append(`${countrydata[0].confirmed}`)
-        $("#totalrecovered").append(`${countrydata[0].recovered}`)
-        $("#totaldeaths").append(`${countrydata[0].deaths}`)
-
 
         //converting dates to ISO format
         for (let i of countrydata) {
@@ -50,84 +32,125 @@ function loadLatest() {
         }
 
         $("#datedisplay").append(`${countrydata[0].date}`)
-
-        //get the data for past 7 days
-        let weeklydata = countrydata.slice(0, 7)
-        //reduce date length on x axis by removing YY
-        for (let i of weeklydata) {
-            i.date = moment(i.date, "DD/MM/YY").format("DD/MM")
+        for (let i = 0; i < 30; i++) {
+            $("#dateselect").append(`<option>${countrydata[i].date}</option>`)
         }
-        console.log(weeklydata)
 
-        let maxweekly = weeklydata[0].confirmed
-        let minweekly = weeklydata[6].confirmed
-        //console.log(minweekly)
-        let cf = crossfilter(weeklydata)
-        let weeklyx = cf.dimension(f => f.date)
-        //console.table(weeklyx.top(5))
-        let weeklyy = weeklyx.group().reduceSum(f => f.confirmed)
+        for (let i = 0; i < 30; i++) {
+            if (countrydata[i].date == loaddate) {
+                console.log(countrydata[i])
+                console.log(i)
 
-        dc.lineChart("#linegraphx")
-            .width(380) //make mobile responsive later!
-            .height(250)
-            .dimension(weeklyx)
-            .group(weeklyy)
-            .x(d3.scaleBand())
-            .xUnits(dc.units.ordinal)
-            .xAxisLabel("Date")
-            .y(d3.scaleLinear().domain([minweekly * 0.95, maxweekly * 1.05]))
-            // .yAxisLabel("Cases")
-            .yAxis().ticks(4)
+                let dailyIncrease = parseInt(countrydata[i].confirmed) - parseInt(countrydata[i+1].confirmed)
+                let dailyRecovered = parseInt(countrydata[i].recovered) - parseInt(countrydata[i+1].recovered)
+                let dailyDeaths = parseInt(countrydata[i].deaths) - parseInt(countrydata[i+1].deaths)
+                //console.log(countrydata)
 
-        dc.renderAll()
-
-
-        let recoveryrate = ((countrydata[0].recovered / countrydata[0].confirmed) * 100).toFixed(2)
-        let remainderr = 100 - recoveryrate
-        //recovery rates chart
-        new Chart(document.getElementById("rrdonut"), {
-            type: 'doughnut',
-            data: {
-                labels: [],
-                datasets: [
-                    {
-                        backgroundColor: ["green", "grey"],
-                        data: [recoveryrate, remainderr]
-                    }
-                ]
-            },
-            options: {
-                events: [],
-                title: {
-                    display: true,
-                    text: 'Recovery Rate' + " " + recoveryrate + "%"
+                $("#ctoday").append(`${dailyIncrease}`)
+                if (dailyIncrease > 0) {
+                    $("#ctoday").append(`<i class="fas fa-angle-double-up red"></i>`)
                 }
-            }
-        });
 
-
-        let deathrate = ((countrydata[0].deaths / countrydata[0].confirmed) * 100).toFixed(2)
-        let remainderd = 100 - recoveryrate
-        //death rates chart
-        new Chart(document.getElementById("drdonut"), {
-            type: 'doughnut',
-            data: {
-                labels: [],
-                datasets: [
-                    {
-                        backgroundColor: ["red", "grey"],
-                        data: [deathrate, remainderd]
-                    }
-                ]
-            },
-            options: {
-                events: [],
-                title: {
-                    display: true,
-                    text: 'Death Rate' + " " + deathrate + "%"
+                $("#rtoday").append(`${dailyRecovered}`)
+                if (dailyRecovered > 0) {
+                    $("#rtoday").append(`<i class="fas fa-angle-double-up green"></i>`)
                 }
+
+                $("#dtoday").append(`${dailyDeaths}`)
+                if (dailyDeaths > 0) {
+                    $("#dtoday").append(`<i class="fas fa-angle-double-up red"></i>`)
+                }
+
+                $("#totalconfirmed").append(`${countrydata[i].confirmed}`)
+                $("#totalrecovered").append(`${countrydata[i].recovered}`)
+                $("#totaldeaths").append(`${countrydata[i].deaths}`)
+
+
+
+
+
+                //get the data for past 7 days
+                let weeklydata = countrydata.slice(i,i+7)
+                //reduce date length on x axis by removing YY
+                for (let i of weeklydata) {
+                    i.date = moment(i.date, "DD/MM/YY").format("DD/MM")
+                }
+                console.log(weeklydata)
+
+                let maxweekly = weeklydata[0].confirmed
+                let minweekly = weeklydata[6].confirmed
+                //console.log(minweekly)
+                let cf = crossfilter(weeklydata)
+                let weeklyx = cf.dimension(f => f.date)
+                //console.table(weeklyx.top(5))
+                let weeklyy = weeklyx.group().reduceSum(f => f.confirmed)
+
+                dc.lineChart("#linegraphx")
+                    .width(380) //make mobile responsive later!
+                    .height(250)
+                    .dimension(weeklyx)
+                    .group(weeklyy)
+                    .x(d3.scaleBand())
+                    .xUnits(dc.units.ordinal)
+                    .xAxisLabel("Date")
+                    .y(d3.scaleLinear().domain([minweekly * 0.95, maxweekly * 1.05]))
+                    // .yAxisLabel("Cases")
+                    .yAxis().ticks(4)
+
+                dc.renderAll()
+
+
+                let recoveryrate = ((countrydata[i].recovered / countrydata[i].confirmed) * 100).toFixed(2)
+                let remainderr = 100 - recoveryrate
+                //recovery rates chart
+                new Chart(document.getElementById("rrdonut"), {
+                    type: 'doughnut',
+                    data: {
+                        labels: [],
+                        datasets: [
+                            {
+                                backgroundColor: ["green", "grey"],
+                                data: [recoveryrate, remainderr]
+                            }
+                        ]
+                    },
+                    options: {
+                        events: [],
+                        title: {
+                            display: true,
+                            text: 'Recovery Rate' + " " + recoveryrate + "%"
+                        }
+                    }
+                });
+
+
+                let deathrate = ((countrydata[i].deaths / countrydata[i].confirmed) * 100).toFixed(2)
+                let remainderd = 100 - recoveryrate
+                //death rates chart
+                new Chart(document.getElementById("drdonut"), {
+                    type: 'doughnut',
+                    data: {
+                        labels: [],
+                        datasets: [
+                            {
+                                backgroundColor: ["red", "grey"],
+                                data: [deathrate, remainderd]
+                            }
+                        ]
+                    },
+                    options: {
+                        events: [],
+                        title: {
+                            display: true,
+                            text: 'Death Rate' + " " + deathrate + "%"
+                        }
+                    }
+                });
             }
-        });
+        }
+
+
+
 
 
 
@@ -139,11 +162,14 @@ function loadLatest() {
 }//function loadlatest end
 
 
+
 //loadonclick
 function getData() {
     let countryselected = $("#countryselect").val()
-    console.log(countryselected)
+    let dateselected = $("#dateselect").val()
+    //console.log(countryselected)
     countryname = countryselected
+    loaddate = dateselected
     loadLatest()
 }
 
--- script.js
+++ script.js
@@ -16,6 +16,7 @@ $(function () {
 
     })//axios for country list
 
+    
 
 
 
